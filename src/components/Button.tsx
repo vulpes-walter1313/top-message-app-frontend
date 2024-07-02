1839057850305e5e321bed3006795fa0350f739d
Walter Fritsche <wfritsche95@protonmail.com>
2024-07-02 11:24:32 -0500

    added button to variants

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,31 +1,44 @@
-import React from 'react'
-import { cn } from '../lib/utils';
+import React from "react";
+import { cn } from "../lib/utils";
+import { Link } from "@tanstack/react-router";
 
 type ButtonProps = {
   as: "link" | "button";
   children: React.ReactNode | string;
-  href: string;
-  variant: "solid" | "outline"
-}
-export default function Button({as, children, href, variant}: ButtonProps) {
-  let styles = "py-2 px-6";
-  
+  href?: string;
+  variant: "solid" | "outline";
+};
+export default function Button({ as, children, href, variant }: ButtonProps) {
+  let styles = "py-2 px-6 block leading-none";
+
   switch (variant) {
     case "solid":
-      styles = cn(styles, "text-zinc-50 bg-emerald-500 font-semibold rounded-full text-lg");
+      styles = cn(
+        styles,
+        "text-zinc-50 bg-emerald-500 font-semibold rounded-full text-lg flex justify-center items-center",
+      );
       break;
     case "outline":
-      styles = cn(styles, "text-emerald-500 border-2 border-emerald-500 font-semibold rounded-full text-lg");
+      styles = cn(
+        styles,
+        "text-emerald-500 border-2 border-emerald-500 font-semibold rounded-full text-lg",
+      );
       break;
     default:
       break;
   }
   if (as === "link") {
     return (
-      <a href={href} className={styles}>{children}</a>
-    )
+      <Link to={href} className={styles}>
+        {children}
+      </Link>
+    );
   }
-  return (
-    <div>Button</div>
-  )
+  if (as === "button") {
+    return (
+      <button className={styles} type="submit">
+        {children}
+      </button>
+    );
+  }
 }
