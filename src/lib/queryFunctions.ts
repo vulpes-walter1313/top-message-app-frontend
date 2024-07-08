8366b43822f7c109b328ca7a667ac81e4afb9fc9
Walter Fritsche <wfritsche95@protonmail.com>
2024-07-08 11:56:14 -0500

    created function for useQuery and useMutation for specific chat pages

--- src/lib/queryFunctions.ts
+++ src/lib/queryFunctions.ts
@@ -29,3 +29,74 @@ export async function createChattRoom({
     throw new Error("Some error occured when posting a new chatt");
   }
 }
+
+export async function getChatInfo(chatId: string) {
+  const rawRes = await fetch(`http://localhost:3000/chats/${chatId}`, {
+    method: "GET",
+    credentials: "include",
+  });
+  const res = await rawRes.json();
+  if (res.success) {
+    return res;
+  } else {
+    throw new Error(`Error fetching chatinfo for chat: ${chatId}`);
+  }
+}
+
+export async function getJoinedChatsInfo() {
+  const rawRes = await fetch(
+    "http://localhost:3000/chats?type=joined&limit=10&page=1",
+    {
+      method: "GET",
+      credentials: "include",
+    },
+  );
+  const res = await rawRes.json();
+  if (res.success) {
+    return res;
+  } else {
+    throw new Error("Error in fetching joined chats");
+  }
+}
+
+export async function getChatMessages(chatId: string) {
+  const rawRes = await fetch(
+    `http://localhost:3000/chats/${chatId}/messages?limit=50&page=1`,
+    {
+      method: "GET",
+      credentials: "include",
+    },
+  );
+
+  const res = await rawRes.json();
+  if (res.success) {
+    return res;
+  } else {
+    throw new Error("Error in loading messages");
+  }
+}
+
+type CreateChatMessateProps = {
+  chatId: string;
+  content: string;
+};
+export async function createChatMessage({
+  chatId,
+  content,
+}: CreateChatMessateProps) {
+  const data = { content };
+  const rawRes = await fetch(`http://localhost:3000/chats/${chatId}/messages`, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  const res = await rawRes.json();
+  if (res.success) {
+    return res;
+  } else {
+    throw new Error("Error is sending message to chat.");
+  }
+}
