4a97aebc9b0fce1e891480c9a1e8e09405fc66a3
Walter Fritsche <wfritsche95@protonmail.com>
2024-07-10 10:23:15 -0500

    fixed pagination functionality

--- src/components/PaginateButtons.tsx
+++ src/components/PaginateButtons.tsx
@@ -3,8 +3,13 @@ import { cn } from "../lib/utils";
 type PaginateButtonsProps = {
   currentPage: number;
   totalPages: number;
+  setDesiredPage: React.Dispatch<React.SetStateAction<number>>;
 };
-function PaginateButtons({ currentPage, totalPages }: PaginateButtonsProps) {
+function PaginateButtons({
+  currentPage,
+  totalPages,
+  setDesiredPage,
+}: PaginateButtonsProps) {
   const pageNumbers: number[] = [];
   for (let i = 1; i <= totalPages; i++) {
     pageNumbers.push(i);
@@ -17,6 +22,7 @@ function PaginateButtons({ currentPage, totalPages }: PaginateButtonsProps) {
             "min-h-11 min-w-11 rounded-md border border-zinc-950/15 bg-zinc-50 p-2",
             { "bg-emerald-500 text-emerald-50": num === currentPage },
           )}
+          onClick={() => setDesiredPage(num)}
         >
           {num}
         </button>
