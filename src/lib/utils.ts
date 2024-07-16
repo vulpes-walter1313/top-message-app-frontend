528cd7f423636aa916a5ecc9e16a983f515b6b7d
Walter Fritsche <wfritsche95@protonmail.com>
2024-07-16 10:33:11 -0500

    added helper function to get the right BE url

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge";
 export function cn(...args: ClassValue[]) {
   return twMerge(clsx(args));
 }
+
+export function getBackendUrl() {
+  if (import.meta.env.PROD) {
+    return import.meta.env.VITE_PROD_BACKEND_URL!;
+  } else {
+    return import.meta.env.VITE_DEV_BACKEND_URL!;
+  }
+}
