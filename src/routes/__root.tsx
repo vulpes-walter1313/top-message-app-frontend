6d8fca864ad0fdf3bc7b1d838371b25bf3540335
Walter Fritsche <wfritsche95@protonmail.com>
2024-07-22 10:45:06 -0500

    made the routerDEvTools only render in development.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,22 @@
+import React, { Suspense } from "react";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+// import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Navbar from "../components/Navbar";
 
 export const Route = createRootRoute({
   component: RootLayout,
 });
+const TanStackRouterDevtools = import.meta.env.PROD
+  ? () => null // Render nothing in production
+  : React.lazy(() =>
+      // Lazy load in development
+      import("@tanstack/router-devtools").then((res) => ({
+        default: res.TanStackRouterDevtools,
+        // For Embedded Mode
+        // default: res.TanStackRouterDevtoolsPanel
+      })),
+    );
 
 function RootLayout() {
   return (
@@ -13,7 +24,9 @@ function RootLayout() {
       <Navbar />
       <hr />
       <Outlet />
-      <TanStackRouterDevtools />
+      <Suspense>
+        <TanStackRouterDevtools />
+      </Suspense>
       <ReactQueryDevtools />
     </>
   );
